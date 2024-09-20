067dac4c5bf66ec4110774452c6b5f00ef2b4cb7
jjkay03 <61110962+jjkay03@users.noreply.github.com>
2024-09-20 22:12:37 +0200

    Plugin main logic

--- src/main/java/com/jjkay03/farmprotect/FarmProtect.kt
+++ src/main/java/com/jjkay03/farmprotect/FarmProtect.kt
@@ -1,13 +1,37 @@
 package com.jjkay03.farmprotect
 
+import org.bukkit.Material
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.block.Action
+import org.bukkit.event.entity.EntityInteractEvent
+import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.plugin.java.JavaPlugin
 
-class FarmProtect : JavaPlugin() {
+class FarmProtect : JavaPlugin(), Listener {
+
+    // Plugin startup logic
     override fun onEnable() {
-        // Plugin startup logic
+        // Register events
+        server.pluginManager.registerEvents(this, this)
     }
 
-    override fun onDisable() {
-        // Plugin shutdown logic
+    // Protect from mobs
+    @EventHandler
+    fun onEntityInteract(event: EntityInteractEvent) {
+        if (event.block.type == Material.FARMLAND) {
+            event.isCancelled = true
+        }
     }
+
+    // Protect from players
+    @EventHandler
+    fun onPlayerInteract(event: PlayerInteractEvent) {
+        if (event.action == Action.PHYSICAL && event.clickedBlock?.type == Material.FARMLAND) {
+            event.isCancelled = true
+        }
+    }
+
+    // Plugin shutdown logic
+    override fun onDisable() {}
 }
