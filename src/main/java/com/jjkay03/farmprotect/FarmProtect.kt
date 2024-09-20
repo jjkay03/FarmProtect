6ec3b88a322f608ce1b1da77ff824e7e8b56bc81
jjkay03 <61110962+jjkay03@users.noreply.github.com>
2024-09-20 21:51:06 +0200

    Creating project

---
+++ src/main/java/com/jjkay03/farmprotect/FarmProtect.kt
@@ -0,0 +1,13 @@
+package com.jjkay03.farmprotect
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class FarmProtect : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
